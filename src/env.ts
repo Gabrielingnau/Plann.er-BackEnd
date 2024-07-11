9d92d3db958de5859c85de34f375e3d7e7e2a84b
Gabriel <gabertrol@gmail.com>
2024-07-11 16:55:18 -0300

    first commit

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,6 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
-  DATABASE_URL: z.string(),
   API_BASE_URL: z.string(),
   WEB_BASE_URL: z.string(),
   PORT: z.coerce.number().default(3336),
